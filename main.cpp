e4f5d89dad24146ba677cfc838ad78a5b0500fa5
Eyu <78002893+Eyu-148@users.noreply.github.com>
2021-09-26 15:10:18 -0400

    update new project1

--- main.cpp
+++ main.cpp
@@ -1,57 +1,57 @@
-/** cs124 project1 by Eyu on 5/30/21 */
+/** cs124 project1 by Eyu on 9/12/21 */
 
 #include <vector>
 #include <iostream>
 #include "Course.h"
 
+using namespace std;
 /**
  * Main
  */
 int main() {
 
-    std::vector<Course> course;
-    std::string fn = "../uvm_fall2021_courses.csv";
+    vector<Course> course;
+    string fn = "../uvm_fall2021_courses.csv";
 
     if (loadFromFile(fn, course)) {
-        std::cout << course.size()
-                  << " records read from file" << std::endl;
+        cout << course.size()
+                  << " records read from file" << endl;
         int firstId = course.front().getRowId();
-        std::cout << "The first row ID read was " << firstId << std::endl;
+        cout << "The first row ID read was " << firstId << endl;
         int lastId = course.back().getRowId();
-        std::cout << "The last row ID read was " << lastId << std::endl;
+        cout << "The last row ID read was " << lastId << endl;
     } else {
-        std::cout << "Something went wrong." << std::endl;
+        cout << "Something went wrong." << endl;
     }
 
     // Run tests to verify everything is working correctly.
     CourseTest();
 
-    // Check for bad data or incorrectly set fields.
-    validFields(course);
-
     // give the number of the courses without instructor
     needInstructor(course);
+    ifSeatAvailable(course);
+    maxEnrollment(course);
 
     // Print a few object to test overload of <<
-    std::cout << std::endl;
+    cout << endl;
 
-    std::cout << std::left
-              << std::setw(7) << "RowId"
-              << std::setw(8) << "Subject"
-              << std::setw(40) << "Title"
-              << std::setw(7) << "CRN"
-              << std::setw(9) << "College"
-              << std::setw(5) << "Max"
-              << std::setw(5) << "Cur"
-              << std::setw(25) << "Instructor"
-              << std::setw(25) << "Email"
-              << std::endl;
+    cout << left
+              << setw(7) << "RowId"
+              << setw(8) << "Subject"
+              << setw(40) << "Title"
+              << setw(7) << "CRN"
+              << setw(9) << "College"
+              << setw(5) << "Max"
+              << setw(5) << "Cur"
+              << setw(25) << "Instructor"
+              << setw(25) << "Email"
+              << endl;
 
-    std::cout << std::string(120, '-') << std::endl;
-    for (int i = 0; i < 7; i++) {
-        std::cout << course.at(i) << std::endl;
+    cout << string(120, '-') << endl;
+    for (int i = 0; i < 20; i++) {
+        cout << course.at(i) << endl;
     }
-    std::cout << "* max = max enrollment, cur=current enrollment" << std::endl
-              << std::endl;
+    cout << "* max = max enrollment, cur=current enrollment" << endl
+              << endl;
 
 }
